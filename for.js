dcc0ad459ce3eeb54953244055aa896e7d1eb59f
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2026-03-23 18:46:11 -0300

    for

--- for.js
+++ for.js
@@ -1,4 +1,7 @@
+const frutas = ['Maçã', 'Pêra', 'Uva']
 
-for (let i = 0; i < 0; i++) {
-    
+for (let i = 0; i < frutas.length; i++) {
+    // const pI = i % 2 === 0 ? 'par' : 'ímpar';
+    // console.log(i, pI)
+    console.log(`Índice ${i}`,frutas[i])
 }
