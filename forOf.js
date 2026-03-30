e4a1033ac6877f8cfcec6dbd7655dcd9af86b8a9
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2026-03-30 17:48:42 -0300

    for of

--- forOf.js
+++ forOf.js
@@ -1,8 +1,22 @@
-const nome = 'Steve Harris';
+const nomes = ['Steve Harris', 'Bruce Dickinson', 'Dave Murry'];
 
 // for (let i = 0; i < nome.length; i++) {
 //     console.log(nome[i]) 
 // }
-for (let i in nome) {
-    console.log(nome[i])
-}
+
+// for (let i in nome) {
+//     console.log(nome[i])
+// }
+
+// for (let valor of nome) {
+//     console.log(valor)
+// }
+
+// Arrow function
+nomes.forEach((valor, indice) => {
+    console.log(valor, indice)
+});
+
+nomes.forEach(function(valor, indice) {
+    console.log(valor, indice)
+});
