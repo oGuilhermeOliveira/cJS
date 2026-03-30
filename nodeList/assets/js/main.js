6dde5403eb5ecec760aa380bbe3745e5ee8bfd52
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2026-03-30 18:39:46 -0300

    node list

---
+++ nodeList/assets/js/main.js
@@ -0,0 +1,10 @@
+const ps = document.querySelectorAll('.paragrafos p');
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColorBody = estilosBody.backgroundColor;
+console.log(backgroundColorBody)
+
+for (let p of ps) {
+    p.style.backgroundColor = backgroundColorBody;
+    p.style.color = '#fff';
+}
