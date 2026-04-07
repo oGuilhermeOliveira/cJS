535d9e3a8b6f05dc6442f2f99fcc1b7b99b47cf1
oGuilhermeOliveira <guilhermeolsnto@gmail.com>
2026-04-07 17:20:23 -0300

    try & catch

--- tryCatch.js
+++ tryCatch.js
@@ -30,9 +30,28 @@ try {
 }  */
 
 function retornaHora(data) {
-    if (!(data instanceof Date)) {
-        console.log('Não é')
+    if (data && !(data instanceof Date)) {
+        throw new TypeError("Esperando instancia de Date.")
     }
+
+    if (!data) {
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false
+    })
 }
 
-retornaHora(new Date());
+try {
+    const data = new Date('01-01-1970 12:58:12');
+    const hora = retornaHora();
+    console.log(hora)
+} catch(e) {
+    console.log(e)
+} finally {
+    console.log('Tenha um bom dia')
+}
+
